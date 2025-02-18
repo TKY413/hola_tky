f3d257c7c66b4b5a2ad8b01c38eb6e94d312412c
TKY413 <pixel413.dev@gmail.com>
2025-02-18 22:22:12 +0900

    #main_features Gemの処理本体やテストコードを中心に実装

--- lib/hola_tky.rb
+++ lib/hola_tky.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "hola_tky/version"
+require "securerandom"
 
 module HolaTky
+  def self.generate
+    SecureRandom.uuid
+  end
+
   class Error < StandardError; end
-  # Your code goes here...
 end
